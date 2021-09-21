5bd23df0d5f01727067c7a7bd0fcb54cbd88937a
Pierre12412 <pierre.bellegueule@gmx.fr>
2021-09-21 13:06:21 +0200

    models

--- src/SoftDesk/settings.py
+++ src/SoftDesk/settings.py
@@ -70,6 +70,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SoftDesk.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+        'rest_framework.authentication.TokenAuthentication'
+    ),
+    'DEFAULT_PERMISSION_CLASSES' : (
+        'rest_framework.permissions.IsAuthenticated'
+    )
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
---
+++ src/issues/api/serializers.py
@@ -0,0 +1,2 @@
+from rest_framework import serializers
+from issues.models import This
