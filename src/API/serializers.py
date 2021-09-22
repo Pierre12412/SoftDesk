ad9927879cd116a32157432a0e39f6226cc97828
Pierre12412 <pierre.bellegueule@gmx.fr>
2021-09-22 12:58:01 +0200

    API Projects

--- src/API/serializers.py
+++ src/API/serializers.py
@@ -7,14 +7,10 @@ from accounts.models import CustomUser
 
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
-    author_user_id = serializers.SlugRelatedField(queryset=CustomUser.objects.all(), slug_field='id', write_only=True)
     class Meta:
         model = Projects
         fields = ('title','description','type','project_id','author_user_id')
-        read_only_fields = ('id',)
-
-    def validate_author(self, value):
-        return self.context['request'].user
+        read_only_fields = ('author_user_id',)
 
 
 class RegisterSerializer(serializers.ModelSerializer):
