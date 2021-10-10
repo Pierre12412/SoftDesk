1789b7cf7ab5bdc63ee942b839362ebc304b8a65
Pierre BELLEGUEULE <78017882+Pierre12412@users.noreply.github.com>
2021-10-10 20:32:54 +0200

    Update settings.py

--- src/SoftDesk/settings.py
+++ src/SoftDesk/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y4ex&vt!y&mr2(@0h!e--6d#(azfm5@s6npdltfegin@0i(9sq'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
